5ba3183e90a5615dc5af320e8bfe12e323f40ac8
Leonardo Viana <leonardoviana00@hotmail.com>
2021-07-01 10:42:07 -0300

    add router

---
+++ src/routes.ts
@@ -0,0 +1,73 @@
+import { Router } from "express";
+import { v4 as uuid } from "uuid";
+import { ensureAuthenticated } from "./middleware"
+
+const router = Router();
+
+interface IProductsDTO {
+    name: string;
+    description: string;
+    price: number;
+    id: string;
+}
+
+const products: IProductsDTO[] = [];
+
+router.get("/products/findByName", (request, response) => {
+    const { name } = request.query;
+    const product = products.filter((p) => p.name.includes(String(name)));
+    return response.json(product);
+});
+
+router.get("/products/:id", (request, response) => {
+    const { id } = request.params;
+    const product = products.find((product) => product.id === id);
+    return response.json(product);
+});
+
+router.post("/products", ensureAuthenticated, (request, response) => {
+    const { name, description, price } = request.body;
+
+    const productAlreadyExists = products.find(
+        (product) => product.name === name
+    );
+
+    if (productAlreadyExists){
+        return response.status(400).json({ message: "Product Already exists!" });
+    }
+
+    const product: IProductsDTO = {
+        description,
+        name,
+        price,
+        id: uuid(),
+    };
+
+    products.push(product);
+
+    return response.json(product);
+});
+
+router.put("/products/:id", ensureAuthenticated, (request, response) => {
+    const { id } = request.params;
+    const { name, description, price } = request.body;
+
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    if (productIndex === -1) {
+        return response.status(400).json({ message: "Product doesn't exists!"});
+    }
+
+    const product: IProductsDTO = Object.assign({
+        id,
+        name,
+        description,
+        price,
+    });
+
+    products[productIndex] = product;
+
+    return response.json(product);
+})
+
+export { router }
